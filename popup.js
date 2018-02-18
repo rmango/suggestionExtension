59d20fad197b7458697d27f585dcefcfe84b62d7
rmango <morgan-graham@live.com>
2018-02-17 17:13:31 -0800

    can get text of what hovered over

--- popup.js
+++ popup.js
@@ -1,4 +1,22 @@
 // \\$\\s?\\d+\\.?\\d{0,2}
+
+/*chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+    console.log(response.farewell);
+  });
+
+chrome.extension.getBackgroundPage().console.log("hi");
+/*document.addEventListener("DOMContentLoaded", function() {
+    console.log("im running");
+
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.executeScript({
+      code: 'document.body.style.backgroundColor="red"'
+    });
+  });
+
+*/
 //className id
 var prevClassNm = "";
 
@@ -12,7 +30,9 @@ document.addEventListener("mousemove", function(e) {
     if(prevDom != null) {
         prevDom.classList.remove(prevClassNm);
     }
-    console.log("class list" + srcElement.classList);
+    console.log("text: " + srcElement.childNodes[0].nodeValue);
+
+    //console.log("class list" + srcElement.classList);
     srcElement.classList.add(prevClassNm);
 
     prevDom = srcElement;
@@ -20,3 +40,7 @@ document.addEventListener("mousemove", function(e) {
         //display popup
     }
 }, false);
+
+/*document.addEventListener('onhover', function () {
+    alert("hovering");
+});*/
