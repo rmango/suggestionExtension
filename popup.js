544c60d4aaa402ecd7a262b5b3278cb244ba939d
rmango <morgan-graham@live.com>
2018-02-17 20:17:13 -0800

    can highlight elements

--- popup.js
+++ popup.js
@@ -18,33 +18,57 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 
 */
 //className id
-var prevClassNm = "crx_mouse_visited";
+var prevClassNm = "popup";
 
 var prevDom = null;
 
 //mouse listener for move event
-document.addEventListener("mousemove", function(e) {
+document.addEventListener("mousemove", function (e) {
     var scrolledPrice;
     //element that user is scrolling over
     var srcElement = e.srcElement;
-    
-    if(prevDom != null) {
+
+    if (prevDom != null) {
         prevDom.classList.remove(prevClassNm);
     }
     var scrolledText = srcElement.childNodes[0].nodeValue;
     console.log("text: " + scrolledText);
     var pattern = new RegExp("\\$\\s?\\d+\\.?\\d{0,2}?");
-    if(scrolledText.match(pattern)) {
+    if (scrolledText.match(pattern)) {
         console.log("found a price");
         scrolledPrice = scrolledText.match(pattern);
         console.log("price: " + scrolledPrice);
+
+        //circle through children to see if span has already been added
+        var alreadyAdded = false;
+        if (srcElement.hasChildNodes) {
+            console.log(srcElement.childNodes);
+            for (var i = 0; i < srcElement.childNodes.length; i++) {
+                console.log(srcElement.childNodes[i].classList);
+                if (srcElement.childNodes[i].classList.contains("popup")) {
+                    alreadyAdded = true;
+                    i = srcElement.childNodes.length;
+                }
+            }
+        }
+
+        //show popup
+        if (!alreadyAdded) {
+            var popupSpan = document.createElement("span");
+            popupSpan.innerHTML = "This is the price: " + scrolledPrice;
+            popupSpan.classList += "popuptext";
+            popupSpan.id = "myPopup";
+            srcElement.appendChild(popupSpan);
+        }
+
+
     }
 
     //console.log("class list" + srcElement.classList);
     srcElement.classList.add(prevClassNm);
 
     prevDom = srcElement;
-    prevDom.onmouseover = function() {
+    prevDom.onmouseover = function () {
         //display popup
     }
 }, false);
