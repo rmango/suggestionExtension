ad51f12ae9a0c7bf3d72a0e7fdd3a64191d894ab
rmango <morgan-graham@live.com>
2018-02-17 17:09:28 -0800

    kind of broken js

--- popup.js
+++ popup.js
@@ -1,1 +1,22 @@
+// \\$\\s?\\d+\\.?\\d{0,2}
+//className id
+var prevClassNm = "";
 
+var prevDom = null;
+
+//mouse listener for move event
+document.addEventListener("mousemove", function(e) {
+    console.log("hi");
+    var srcElement = e.srcElement;
+    
+    if(prevDom != null) {
+        prevDom.classList.remove(prevClassNm);
+    }
+    console.log("class list" + srcElement.classList);
+    srcElement.classList.add(prevClassNm);
+
+    prevDom = srcElement;
+    prevDom.onmouseover = function() {
+        //display popup
+    }
+}, false);
